a4f4dd4efe3352bf560c9752a4b0c4010313015d
izwel <izwel@izweldeMacBook-Pro.local>
2016-02-07 13:20:24 +0800

    math

---
+++ math/math.js
@@ -0,0 +1,10 @@
+/**
+ * Created by izwel on 16/2/7.
+ */
+Math.sign(-5) // -1
+console.log(Math.sign('5')); // +1
+Math.sign(0) // +0
+Math.sign('-0') // -0
+Math.sign(NaN) // NaN
+
+console.log(Math.fround(0.3)) //0.30000001192092896
