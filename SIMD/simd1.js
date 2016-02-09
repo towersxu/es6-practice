4df6b44b46eaffcc5b5ea9565ac82e05347760fa
taox mac <987427795@qq.com>
2016-02-09 12:54:52 +0800

    == 运算逻辑

---
+++ SIMD/simd1.js
@@ -0,0 +1,3 @@
+var a = SIMD.Float32x4(1, 2, 3, 4);
+var b = SIMD.Float32x4(5, 6, 7, 8);
+var c = SIMD.Float32x4.add(a, b); // Float32x4[6, 8, 10, 12]
---
+++ reg/reg.js
@@ -0,0 +1,9 @@
+/**
+ * Created by izwel on 16/2/9.
+ */
+var s = 'aa_aaa_aa';
+var r1 = /a+/g;
+//var r2 = /a+/y;
+
+r1.exec(s) // ["aaa"]
+r2.exec(s) // ["aaa"]
